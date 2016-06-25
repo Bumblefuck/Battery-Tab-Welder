bdc2e52cd8bda0f7d503d3840be175c89d96455a
Ian Katz <iakatz@paypal.com>
2016-06-25 09:31:54 -0400

    Refactor original battery tab welder program

--- battery_tab_welder/battery_tab_welder.ino
+++ battery_tab_welder/battery_tab_welder.ino
@@ -2,77 +2,106 @@
 	MicroView Arduino Library
 	Copyright (C) 2014 GeekAmmo
  MicroView Battery Tab Welder
+ 
  CopyLEFT (use and abuse as you see fit) 2016 AvE
+ 
  This is an Arduino sketch for use with a battery tab / spot welder
  it uses the microview from sparkfun. The microview is a ATTiny arduino-esq board with a built in OLED display.
  tested and working on microview from sparkfun and programmer from sparkfun and Arduino IDE 1.6.8
  Noise from solenoids sometimes cause uview to hang. Use a momentary "dead man" switch as precaution. Or not. Be sure to get vid of resulting fire.
 */
 #include < MicroView.h >
-const int sensorPin = A0; // pin 7 on uview select the input pin for the potentiometer
-const int weldPin = 2; // pin 11 on uview  select the pin for the LED
-int sensorValue = 0; // variable to store the value coming from the potentiometer
-const int buttonPin = 5; // pin 13 on uview
-int energize = 0; // variable to store the value of the buttonPin that starts the weld pulse sequence
 
-MicroViewWidget * vWidget2; // this is a vertical star trek sick bay (the original) bar graph with triangle pointer
+// pins
+const int sensorPin = A0; // pin 7 on uview - the input pin for the pulse time potentiometer
+const int weldPin = 2;    // pin 11 on uview - the pin for the LED
+const int buttonPin = 5;  // pin 13 on uview
+
+// customize your welder
+const int PULSE_MILLIS_MIN = 200;
+const int PULSE_MILLIS_MAX = 2000;
+
+// state variables
+int sensorValue = 0;       // for the value coming from the potentiometer
+MicroViewWidget* vWidget2; // this is a vertical star trek sick bay (the original) bar graph with triangle pointer
+
+void displayPulseTime() {
+    uView.clear(PAGE);
+    delay(2);
+    vWidget2->reDraw(); // this re-draws the bar gauge, otherwise it acts weird
+}
 
 void setup() {
-    uView.begin();
-    uView.clear(PAGE);
     pinMode(weldPin, OUTPUT);
     pinMode(buttonPin, INPUT);
     digitalWrite(weldPin, LOW); // this is just to make sure the weldPin is LOW before anything gets started
-    vWidget2 = new MicroViewSlider(10, 0, 200, 2000, WIDGETSTYLE3); // this sets the position of the slider (horizontal, vertical, low value, high value)
+
+    // set up uView
+    uView.begin();
+    vWidget2 = new MicroViewSlider(10,                // horizontal position
+                                   0,                 // vertical position
+                                   PULSE_MILLIS_MIN,  // low value
+                                   PULSE_MILLIS_MAX,  // high value
+                                   WIDGETSTYLE3); 
+    displayPulseTime();
+}
+
+// read the value from the sensor and convert to the number of milliseconds we'll use for the pulse time. 
+int getSensorValue() {
+    int rawSensorValue = analogRead(sensorPin); 
+
+    return map(rawSensorValue, 
+               0, 1023,                               // input range
+               PULSE_MILLIS_MIN, PULSE_MILLIS_MAX);   // output range
+
+}
+
+// update the OLED display with the desired pulse Time
+void updatePulseTime(int milliseconds) {
+    vWidget2->setValue(milliseconds); // sets the cursor on the bar graph
+    uView.display();                  // enable the write of the previous instruction or similar
+    uView.print(" ms");               // this is to add units (milliseconds) to the bar graph
+    uView.display();
+}
+
+// Display a (delayed) 3....2....1... for the given number of steps
+//    followed by "FIRE!"
+void displayCountdown(int steps) {
+    uView.clear(PAGE);    // remove what was on the screen before
+    delay(2); 
+
+    // display the countdown steps
+    for (int i = steps; i > 0; --i) {
+        uView.print(" " + String(i));
+        uView.display();
+        delay(500);    
+    }
+
+    uView.print("        FIRE!"); // SPARKS, YIKES!
+    uView.display();
+    delay(5);
+}
+
+// FIRE THE PIXIES
+void spotWeld(int pulseTime) {
+    digitalWrite(weldPin, HIGH);  // energize the small 5V PCB solenoid to fire the big 12V automotive solenoid.
+    delay(pulseTime);             // leaves the weldPin energized for the length of time set on the potentiometer
+    digitalWrite(weldPin, LOW);   // turns the weldPin off
+    delay(5);                     // makes sure everything has time to do what it is supposed to
 }
 
 void loop() {
-    // read the value from the sensor:
-    sensorValue = analogRead(sensorPin); // this is the pot to control the pulse time in milliseconds
+    // read and display the value from the sensor:
+    sensorValue = getSensorValue();
+    updatePulseTime(sensorValue);
 
-    sensorValue = map(sensorValue, 0, 1023, 200, 2000); // this converts the analog input value, into the 200 to 2000 milliseconds we'll use for the pulse time. It can be set as long or short as you need. But you'll need to change the microviewslider value to match
     // turn the ledPin off:
 
-    energize = digitalRead(buttonPin); // checks to see if the weld button is pressed and sets the value of energize to LOW or HIGH
-
-    vWidget2->setValue(sensorValue); // sets the cursor on the bar graph to whatever the pot is reading after conversion to milliseconds
-    uView.display(); // dunno what this does but it's in every sketch with the microview. it must enable the write of the previous instruction or similar
-    uView.print(" ms"); // this is to add units (milliseconds) to the bar graph
-    uView.display();
-
-    // we checked the buttonPin previously and set energize to that value; if it's HIGH it'll start the weld sequence
+    // if the weld button is pressed, start the weld sequence then go back to the pulse time display
+    int energize = digitalRead(buttonPin); 
     if (energize == HIGH) {
-        uView.clear(PAGE);
-        delay(2); // clear page
-
-        // countdown
-        uView.print(" 3");
-        uView.display();
-        delay(500);
-
-        // countdown
-        uView.print(" 2");
-        uView.display();
-        delay(500);
-
-        // countdown
-        uView.print(" 1");
-        uView.display();
-        delay(500);
-
-        uView.print("        FIRE!"); // SPARKS, YIKES!
-        uView.display();
-
-        delay(5);
-        digitalWrite(weldPin, HIGH); // energize the small 5V PCB solenoid to fire the big 12V automotive solenoid.
-
-        delay(sensorValue); // leaves the weldPin energized for the length of time set on the potentiometer
-        digitalWrite(weldPin, LOW); // turns the weldPin off
-
-        delay(5); // makes sure everything has time to do what it is supposed to
-
-        uView.clear(PAGE);
-        delay(2);
-        vWidget2->reDraw(); // this re-draws the bar gauge, otherwise it acts weird
+        displayCountdown(3);
+        spotWeld(sensorValue);
+        displayPulseTime();
     }
 }
